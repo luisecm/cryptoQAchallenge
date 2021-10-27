df3a8c6cce12467f581509af6b0c3458d8e38231
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2021-10-26 19:30:42 -0500

    Update BitsoTestCases02.js
    Adding minor improvements to select random values for Relative field and only include valid birth years

--- cypress/integration/BitsoTestCases02.js
+++ cypress/integration/BitsoTestCases02.js
@@ -37,31 +37,34 @@ describe("Bitso Challenge Test Scenario 02", () => {
     //This action will be executed before all test cases from suite
     before(function() {
         //Login to website and then go to add beneficiaries site
-        cy.loginToPage(credentials.user, credentials.password)
-        cy.goToBeneficiaries()     
+        cy.loginToPage(credentials.user, credentials.password).then(() => {
+            cy.goToBeneficiaries().then(() => {
+                cy.validateBeneficiariesPageIsLoaded()
+            })     
+        })
     })
 
     it("Validate invalid PIN scenario after adding beneficiaries", () => {        
         //One test case is created to validate the full scenario
-        cy.validateBeneficiariesPageIsLoaded().then(() => {
-            //fill form values using the command previously created for input boxes
-            cy.fillRandomData("#first_name", faker.name.firstName())
-            cy.fillRandomData("#last_name", faker.name.lastName())
-            cy.fillRandomData("#second_last_name", faker.name.lastName())
-            cy.fillRandomData("#day", getRandomNumberInclusive(1, 28).toString())
-            cy.fillRandomData("#month", faker.date.month())
-            cy.fillRandomData("#year", getRandomNumberInclusive(1900, 2002).toString())
-            cy.fillRandomData("#percentage", getRandomNumberInclusive(1, 100).toString())
-            //selecting the value 'Relative' in dropdown to be able to pass the form fill
-            cy.get('.gLhA-DK > .css-m0do4z > .css-16ljna5 > .css-tdzd0p').select('Relative')
-        }).then(() => {
-            //click on submit button
+        //fill form values using the command previously created for input boxes
+        cy.fillRandomData("#first_name", faker.name.firstName())
+        cy.fillRandomData("#last_name", faker.name.lastName())
+        cy.fillRandomData("#second_last_name", faker.name.lastName())
+        cy.fillRandomData("#day", getRandomNumberInclusive(1, 28).toString()+'{enter}')
+        cy.fillRandomData("#month", faker.date.month()+'{enter}')
+        cy.fillRandomData("#year", getRandomNumberInclusive(1932, 2021).toString()+'{enter}')
+        cy.fillRandomData("#percentage", getRandomNumberInclusive(1, 100).toString())
+        //selecting the value 'Relative' in dropdown to be able to pass the form fill
+        cy.selectKinshipValue(getRandomNumberInclusive(1,3)).then(() => {
+        //click on submit button
             cy.submitRandomData()
-        })
-        //After modal message is displayed, we get a random pin to pass and then click on Confirm
-        cy.confirmBeneficiary(getRandomNumberInclusive(11111, 99999)).then(() => {
+        }).then(() => {
+            //After modal message is displayed, we get a random pin to pass and then click on Confirm
+            cy.confirmBeneficiary(getRandomNumberInclusive(11111, 99999))
+        }).then(() => {
             //Validation of message "Incorrect PIN" and closing modal to finish
-            cy.validateIncorrectMessage()
+            cy.validateIncorrectPinMessage()
         })
     })
+
 })
